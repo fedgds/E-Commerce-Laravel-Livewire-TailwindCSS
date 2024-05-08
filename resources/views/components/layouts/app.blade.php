8645b153c7a8e05d8745700ea8ae87a0e653b3dc
sangla3 <lavansang1264@gmail.com>
2024-05-08 23:29:53 +0700

    layout customer

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,13 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Shopee' }}</title>
-        @vite(['resources/css/appcss', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
     </head>
     <body class="bg-slate-200 dark:bg-slate-700">
+        @livewire('section.navbar')
         <main>
             {{ $slot }}
         </main>
+        @livewire('section.footer')
         @livewireScripts
     </body>
 </html>
